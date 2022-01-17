d79bb3054dbf0d498038304e035e837eaccb16b0
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-17 16:32:59 -0500

    refactoring mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,14 @@
-const tail = require('../tail.js')
-const assertEqual = require('../assertEqual.js')
+const assert = require('chai').assert;
+const tail = require('../tail.js');
 
-const newArray = [1,2,3];
-assertEqual(tail(newArray)[0], 2);
-assertEqual(tail(newArray).length, 2);
-assertEqual(tail(newArray).length, 3);
-assertEqual(tail(newArray).length, 4);
+describe('#tail', () => {
+  it('returns [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]),[2, 3]);
+  });
+  it('returns [2, 3, 4] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it('returns [] for []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
