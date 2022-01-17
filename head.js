4f861dddabe5f8a42c589ee9d45541f939a97147
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-17 10:06:27 -0500

    refactoring and adding module.exports

--- head.js
+++ head.js
@@ -1,22 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: , ${actual}, = , ${expected}`);
-  }
-  else {
-    console.log(`Assertion Failed: , ${actual}, != , ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
-  let head = arr[0]
-  return head
+  let head = arr[0];
+  return head;
 };
 
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Labs", "javascript", "coding"]), "coding");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "undefined");
-assertEqual(head([]), undefined);
+module.exports = head;
