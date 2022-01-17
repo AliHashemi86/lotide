2a52baf81c51ea60354b60d4e87b50b9ba28224a
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-17 16:51:16 -0500

    mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,14 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
-const eqArrays = require('../eqArrays')
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [5]);
-assertArraysEqual(middle([1, 3]), [1]);
-assertArraysEqual(middle([1]), [1]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5]);
+describe('#middle', () => {
+  it('return [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it('return [5] for [3, 4, 5, 6, 7]', () => {
+    assert.deepEqual(middle([3, 4, 5, 6, 7]), [5]);
+  });
+  it('return [] for [4]', () => {
+    assert.deepEqual(middle([4]), []);
+  });
+});
