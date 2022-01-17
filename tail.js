ec7657d339b53e8ed095cd9aeb97d1de1a40d389
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-17 13:14:22 -0500

    refactoring and use module exports

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: , ${actual}, = , ${expected}`);
-  }
-  else {
-    console.log(`Assertion Failed: , ${actual}, != , ${expected}`)
-  }
-};
+const assertEqual = require('./assertEqual')
+
 
 const tail = function(array) {
   let newArray = [];
@@ -15,9 +9,4 @@ const tail = function(array) {
   return newArray;
 };
 
-let newArray = [1,2,3];
-
-assertEqual(tail(newArray)[0], 2);
-assertEqual(tail(newArray).length, 2);
-assertEqual(tail(newArray).length, 3);
-assertEqual(tail(newArray).length, 4);
+module.exports = tail;
