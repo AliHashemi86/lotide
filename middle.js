2f38eb243cf980f7982eea76941b193c3ef6a717
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-01-17 15:11:25 -0500

    refactoring

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: , ${actual}, = , ${expected}`);
-  }
-  else {
-    console.log(`Assertion Failed: , ${actual}, != , ${expected}`);
-  }
-};
-
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(arr3) {
   let finalArr = [];
@@ -37,12 +18,4 @@ const middle = function(arr3) {
     return finalArr;
   }
 
-
-
-assertArrayEqual(middle([1, 2, 3]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [5]);
-assertArrayEqual(middle([1, 3]), [1]);
-assertArrayEqual(middle([1]), [1]);
-assertArrayEqual(middle([1]), []);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5]);
+module.exports = middle;
